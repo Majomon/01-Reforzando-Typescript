7c1839ab1cf8ac9675e03ae7e806b7c592c05ebd
Majomon <mauri.monzon91@gmail.com>
2023-08-08 10:47:03 -0300

    Interfaces de peticioines axios

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,11 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { reqResApi } from "../api/reqRes";
+import { ReqResListado, Usuario } from "../interfaces/reqRes";
 
 export const Usuarios = () => {
+  const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+
   useEffect(() => {
     //Llamado a la Api
     reqResApi
-      .get("/users")
+      .get<ReqResListado>("/users")
       .then((resp) => {
         console.log(resp.data.data);
       })
