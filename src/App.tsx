022f426972a94fb6892b9e75012d5b9bed6bc15a
Majomon <mauri.monzon91@gmail.com>
2023-08-08 14:30:26 -0300

    Todo habilitado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-/* import { TiposBasicos } from "./typescript/TiposBasicos"; */
-/* import { ObjetosLiterales } from "./typescript/ObjetosLiterales"; */
-/* import { Funciones } from "./typescript/Funciones"; */
-/* import { Contador } from "./components/Contador"; */
-/* import { ContadorConHook } from "./components/ContadorConHook"; */
-/* import { Login } from "./components/Login"; */
-/* import { Usuarios } from "./components/Usuarios"; */
+import { TiposBasicos } from "./typescript/TiposBasicos";
+import { ObjetosLiterales } from "./typescript/ObjetosLiterales";
+import { Funciones } from "./typescript/Funciones";
+import { Contador } from "./components/Contador";
+import { ContadorConHook } from "./components/ContadorConHook";
+import { Login } from "./components/Login";
+import { Usuarios } from "./components/Usuarios";
 import { Formularios } from "./components/Formularios";
 
 const App = () => {
@@ -12,13 +12,13 @@ const App = () => {
     <div className="mt-2">
       <h1>Introducción a TS - React</h1>
       <hr />
-      {/*       <TiposBasicos /> */}
-      {/*       <ObjetosLiterales /> */}
-      {/*      <Funciones /> */}
-      {/*   <Contador /> */}
-      {/*       <ContadorConHook /> */}
-      {/*      <Login /> */}
-      {/*  <Usuarios /> */}
+      <TiposBasicos />
+      <ObjetosLiterales />
+      <Funciones />
+      <Contador />
+      <ContadorConHook />
+      <Login />
+      <Usuarios />
       <Formularios />
     </div>
   );
