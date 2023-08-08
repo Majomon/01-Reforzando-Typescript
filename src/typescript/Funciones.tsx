7c40accb496ef9f02bfdceceb7fadc3adecc13ae
Majomon <mauri.monzon91@gmail.com>
2023-08-07 23:08:44 -0300

    Funciones

---
+++ src/typescript/Funciones.tsx
@@ -0,0 +1,12 @@
+export const Funciones = () => {
+  const sumar = (): number => {
+    return "Gola mundo";
+  };
+
+  return (
+    <>
+      <h3>Funciones</h3>
+      <span>El resultado es: {sumar()}</span>
+    </>
+  );
+};
